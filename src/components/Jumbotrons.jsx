dff31e8c6ee12224c065075e26379afabd1118ac
Arif . <humanajaib3317@gmail.com>
2025-02-17 18:36:02 +0700

    commit portofolio web

--- src/components/Jumbotrons.jsx
+++ src/components/Jumbotrons.jsx
@@ -10,7 +10,7 @@ const Jumbotrons = () => {
           className="w-[18rem] rounded-3xl border-5 border-slate-50 xl:float-end xl:me-16 xl:w-[22rem]"
         />
       </div>
-      <div className="mt-7 font-semibold xl:col-span-5 xl:col-start-7 xl:mt-0">
+      <div className="mt-7 xl:col-span-5 xl:col-start-7 xl:mt-0">
         <div className="hidden text-[33px] font-normal xl:block xl:text-start">
           Hello, i'm
         </div>
@@ -24,7 +24,7 @@ const Jumbotrons = () => {
         <div className="flex flex-row items-center justify-center gap-8 xl:justify-start xl:gap-12 xl:text-start">
           <a
             href="/about"
-            className="rounded-lg bg-zinc-900 px-4 py-1 text-xl text-gray-50 hover:opacity-85 lg:px-6 xl:mb-3 dark:bg-slate-200 dark:text-zinc-900"
+            className="rounded-lg bg-zinc-900 px-4 py-1 text-xl font-semibold text-gray-50 hover:opacity-85 lg:px-6 xl:mb-3 dark:bg-slate-200 dark:text-zinc-900"
           >
             About me
           </a>
